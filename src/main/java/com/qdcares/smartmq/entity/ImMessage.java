cf7a7d9f1b40127ef6145662b630a2e682f70373
liuyiqiang <liuyq@qdcares.cn>
2019-10-10 15:41:30 +0800

    add entity and myqsql config

---
+++ src/main/java/com/qdcares/smartmq/entity/ImMessage.java
@@ -0,0 +1,64 @@
+package com.qdcares.smartmq.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @Description: 消息表
+ * @Author: LiuYiQiang
+ * @Date: 15:01 2019/10/10
+ */
+@Entity
+@Table(name = "IM_MESSAGE")
+@Getter
+@Setter
+public class ImMessage {
+
+    /**
+     * 主键
+     */
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false, precision = 11)
+    private Integer id;
+
+    /**
+     * 发送用户ID
+     */
+    @Column(name = "SENDER_ID",precision = 11)
+    private Integer senderId;
+
+    /**
+     * 所属聊天ID
+     */
+    @Column(name = "CHAT_ID",precision = 11)
+    private Integer chatId;
+
+    /**
+     * 全局唯一标识
+     */
+    @Column(name = "UUID",length = 64)
+    private String uuid;
+
+    /**
+     * 消息类型（TEXT:文本；AUDIO:语音；VIDEO:视频；FILE:文件）
+     */
+    @Column(name = "TYPE",length = 32)
+    private String type;
+
+    /**
+     * 消息内容（文本或者文件唯一标识）
+     */
+    @Column(name = "CONTENT",length = 1024)
+    private String content;
+
+    /**
+     * 消息发送时间
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "MESSAGE_TIME")
+    private Date messageTime;
+}
---
+++ src/main/java/com/qdcares/smartmq/entity/ImUserMessage.java
@@ -0,0 +1,45 @@
+package com.qdcares.smartmq.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @Description: 用户消息表（冗余记录用户接收的消息，便于检索）
+ * @Author: LiuYiQiang
+ * @Date: 15:01 2019/10/10
+ */
+@Entity
+@Table(name = "IM_USER_MESSAGE")
+@Getter
+@Setter
+public class ImUserMessage {
+
+    /**
+     * 主键
+     */
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false, precision = 11)
+    private Integer id;
+
+    /**
+     * 用户ID
+     */
+    @Column(name = "USER_ID",precision = 11)
+    private Integer userID;
+
+    /**
+     * 消息ID
+     */
+    @Column(name = "MESSAGE_ID",precision = 11)
+    private Integer messageId;
+
+    /**
+     * 已读状态
+     */
+    @Column(name = "ACKNOWLEDGED",columnDefinition="tinyint default 0")
+    private Boolean acknowledged;
+}
